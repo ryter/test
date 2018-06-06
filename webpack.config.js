11b64a10e7c9e6aa1fc547c0fbdad27411e86398
Ryter <gmtg2009@gmail.com>
2018-06-07 01:39:27 +0300

    add slice

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,115 @@
-let path = require('path');
-let ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageMinPlugin = require('imagemin-webpack-plugin');
+
+const extractHTML = new ExtractTextPlugin('index.html');
+const extractCSS = new ExtractTextPlugin('css/style.css');
 
 const conf = {
-  entry: {
-    app: './src/js/app.js',
-    page: './src/slim/index.slim'
-  },
+  //context: path.resolve(__dirname, 'dist'),
+
+  entry: [
+    './src/js/app.js',
+    './src/slim/index.slim',
+    './src/scss/style.scss'
+  ],
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'js/app.js',
-    publicPath: 'dist/'
+    filename: 'js/build.js',
   },
 
   devServer: {
-    contentBase: path.join(__dirname, '../src'),
-  //  compress: true,
-    port: 9000
+    overlay: true
   },
 
   module: {
     rules: [
         {
           test: /\.slim$/,
-          use: ExtractTextPlugin.extract({
+          use: extractHTML.extract({
             use: [
               {
                 loader: 'html-loader',
                 options: {
                   minimize: false
                 }
-              }, {
+              },{
                 loader: 'slim-lang-loader',
                 options: {
                   slimOptions: {
-                    'pretty': true
+                    //'pretty': true
                   }
                 }
               }
             ]
         })
+      },{
+        test: /\.scss$/,
+        use: extractCSS.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {sourceMap: true}
+            },{
+              loader: 'postcss-loader',
+              options: {sourceMap: true}
+            },{
+              loader: 'sass-loader',
+              options: {sourceMap: true}
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },{
+        test: /\.(png|gif|jpe?g)$/,
+        loaders: [
+            {
+              loader: 'file-loader?name=images/[name].[ext]',
+              options: {
+                publicPath: '../'
+              },
+          },
+          'img-loader',
+        ]
+      },{
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              encoding: 'base64'
+            }
+          },{
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                {
+                  removeViewBox: false
+                }
+              ]
+            }
+          },
+        ]
       }
     ]
   },
 
   plugins: [
-    new ExtractTextPlugin('index.html')
+    new CleanWebpackPlugin(['dist']),
+    extractHTML,
+    extractCSS,
+    new CopyWebpackPlugin (
+      [
+        {from: './src/images', to: 'images'}
+      ], {
+        ignore : [
+          {glob: 'svg/*'}
+        ]
+      }
+    )
   ]
 };
-
-console.log(conf);
 
 module.exports = conf;
